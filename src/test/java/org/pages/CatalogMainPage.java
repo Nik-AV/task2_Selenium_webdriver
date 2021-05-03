789f039d49a93169c352a54fb0ac4c67b6b83fea
Nikita Avvakumov <na.avvakumov@gmail.com>
2021-05-03 20:52:16 +0300

    Parameters added

---
+++ src/test/java/org/pages/CatalogMainPage.java
@@ -0,0 +1,37 @@
+package org.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class CatalogMainPage {
+
+    private WebDriver driver;
+
+    public CatalogMainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//span[@class='catalog-navigation-classifier__item-title-wrapper'][contains(text(), 'Электроника')]")
+    private WebElement electronics;
+
+    public void clickElectronics() {
+        electronics.click(); }
+
+        @FindBy(xpath = "//div[@class='catalog-navigation-list__aside-title'][contains(text(), 'Телевидение')]")
+    private WebElement television;
+
+    public void clickTelevision() {
+        television.click(); }
+
+    @FindBy(xpath = "//span[@class='catalog-navigation-list__dropdown-title'][contains(text(), 'Телевизоры')]")
+    private WebElement tv;
+
+    public void clickTV() {
+        tv.click(); }
+
+
+}
--- src/test/java/org/test/SeleniumTest.java
+++ src/test/java/org/test/SeleniumTest.java
@@ -1,12 +1,17 @@
 package org.test;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.pages.CatalogMainPage;
+import org.pages.OnlinerMainPage;
+import org.pages.TVPage;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.verification.Verification;
+
 import java.util.concurrent.TimeUnit;
 
 public class SeleniumTest  {
@@ -17,10 +22,11 @@ public class SeleniumTest  {
     public static Verification verification;
     public static WebDriver driver;
 
+    @Parameters({"startPage", "chromedriver"})
     @BeforeTest
-    public static void setup() {
+    public static void setup(String startPage, String chromedriver) {
 
-        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        System.setProperty("webdriver.chrome.driver", chromedriver);
 
         driver = new ChromeDriver();
 
@@ -31,63 +37,39 @@ public class SeleniumTest  {
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.get(ConfProperties.getProperty("startPage"));
+        driver.get(startPage);
     }
 
+    @Parameters({"preconditionPrice", "preconditionResolution", "preconditionTile", "preconditionMinInches", "preconditionMaxInches"})
     @Test
-    public void test() throws InterruptedException {
-
-        JavascriptExecutor js = (JavascriptExecutor)driver;
-
-        String URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://www.onliner.by/");
+    public void test(String preconditionPrice, String preconditionResolution, String preconditionTile,String preconditionMinInches, String preconditionMaxInches)  throws InterruptedException{
 
         onlinerMainPage.entryCatalog();
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/");
 
         catalogMainPage.clickElectronics();
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/");
 
         catalogMainPage.clickTelevision();
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/");
 
         catalogMainPage.clickTV();
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/tv");
 
         tvPage.selectSamsung();
-        Thread.sleep(1000);
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/tv?mfr%5B0%5D=samsung");
 
         tvPage.setPrice();
-        Thread.sleep(1000);
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/tv?mfr%5B0%5D=samsung&price%5Bto%5D=1000.00");
 
         tvPage.selectResolution();
-        Thread.sleep(1000);
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/tv?mfr%5B0%5D=samsung&price%5Bto%5D=1000.00&resolution_tv%5B0%5D=1920x1080&resolution_tv%5Boperation%5D=union");
 
         tvPage.selectMinInch();
-        Thread.sleep(1000);
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/tv?mfr%5B0%5D=samsung&price%5Bto%5D=1000.00&diagonal_tv%5Bfrom%5D=400&resolution_tv%5B0%5D=1920x1080&resolution_tv%5Boperation%5D=union");
 
         tvPage.selectMaxInch();
-        Thread.sleep(1000);
-        URL = js.executeScript("return document.URL;").toString();
-        Assert.assertEquals(URL, "https://catalog.onliner.by/tv?mfr%5B0%5D=samsung&price%5Bto%5D=1000.00&diagonal_tv%5Bfrom%5D=400&diagonal_tv%5Bto%5D=500&resolution_tv%5B0%5D=1920x1080&resolution_tv%5Boperation%5D=union");
+
+        Thread.sleep(3000);
 
         SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(verification.verifyTitle(), true);
-        softAssert.assertEquals(verification.verifyResolution(), true);
-        softAssert.assertEquals(verification.verifyPrice(), true);
-        softAssert.assertEquals(verification.verifyInches(), true);
+
+        softAssert.assertEquals(verification.verifyTitle(preconditionTile), true);
+        softAssert.assertEquals(verification.verifyResolution(preconditionResolution), true);
+        softAssert.assertEquals(verification.verifyPrice(preconditionPrice), true);
+        softAssert.assertEquals(verification.verifyInches(preconditionMinInches, preconditionMaxInches), true);
 
     }
 
